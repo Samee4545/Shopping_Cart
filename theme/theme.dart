27cd4c4a579bcf83cbbd283e16ef443e71f1ce31
Muhammad Sameer <116864323+Samee4545@users.noreply.github.com>
2024-11-07 20:52:54 +0500

    Add files via upload

---
+++ theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.light(
+  background: Colors.grey.shade300,
+  primary: Colors.grey.shade200,
+  secondary: Colors.white,
+  inversePrimary: Colors.grey.shade800,
+));
